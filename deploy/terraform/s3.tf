f4083de0d4a3e459e19040082d7a40c74800a25f
erkangz <erkang.zheng@lifeomic.com>
2019-09-26 18:15:35 -0400

    remove deploy

--- deploy/terraform/s3.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "docs_redirect" {
-  bucket = "${var.s3_redirect_bucket_name}"
-  acl    = "public-read"
-
-  website {
-    redirect_all_requests_to = "https://support.jupiterone.io/hc/en-us"
-  }
-}
